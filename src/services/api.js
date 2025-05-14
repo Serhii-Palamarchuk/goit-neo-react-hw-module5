c87b4323e09e1f7be53a8838efbea648ae1dfc51
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-05-14 16:18:40 +0300

    Add API functions for searching movies, retrieving movie details, cast, and reviews

--- src/services/api.js
+++ src/services/api.js
@@ -13,3 +13,26 @@ export const getTrendingMovies = async () => {
   const response = await axios.get(`${BASE_URL}/trending/movie/day`, options);
   return response.data;
 };
+
+export const searchMovies = async query => {
+  const response = await axios.get(
+    `${BASE_URL}/search/movie?query=${encodeURIComponent(query)}&language=en-US&page=1`,
+    options
+  );
+  return response.data;
+};
+
+export const getMovieDetails = async movieId => {
+  const response = await axios.get(`${BASE_URL}/movie/${movieId}`, options);
+  return response.data;
+};
+
+export const getMovieCast = async movieId => {
+  const response = await axios.get(`${BASE_URL}/movie/${movieId}/credits`, options);
+  return response.data;
+};
+
+export const getMovieReviews = async movieId => {
+  const response = await axios.get(`${BASE_URL}/movie/${movieId}/reviews`, options);
+  return response.data;
+};
