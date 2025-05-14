ee2fb672be22bb1b53513d1523b07353cd3ae3fb
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-05-14 16:29:34 +0300

    Enhance MovieList component to display movie posters and adjust CSS for improved layout

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,24 @@
 import { Link, useLocation } from 'react-router-dom';
 import styles from './MovieList.module.css';
 
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w300';
+
 const MovieList = ({ movies }) => {
   const location = useLocation();
 
   return (
     <ul className={styles.list}>
-      {movies.map(({ id, title }) => (
+      {movies.map(({ id, title, poster_path }) => (
         <li key={id} className={styles.item}>
           <Link to={`/movies/${id}`} state={{ from: location }}>
-            {title}
+            {poster_path && (
+              <img
+                src={`${IMG_BASE_URL}${poster_path}`}
+                alt={title}
+                className={styles.poster}
+              />
+            )}
+            <p>{title}</p>
           </Link>
         </li>
       ))}
